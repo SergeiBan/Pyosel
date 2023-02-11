c6d3cbd29237fcee112368ad64f5259de669cd82
Sergei Banshchikov <s.o.banshchikov@yandex.ru>
2023-02-11 23:37:25 +0200

    Adds filtering of the lost feed

--- animals/views.py
+++ animals/views.py
@@ -5,6 +5,7 @@ from animals.serializers import (
     FoundProfileSerializer, OutputFoundProfileSerializer)
 from animals.permissions import IsOwnerOrReadOnly
 from rest_framework.permissions import AllowAny
+from django_filters import rest_framework as filters
 
 
 class AnimalViewSet(viewsets.ModelViewSet):
@@ -17,16 +18,17 @@ class AnimalViewSet(viewsets.ModelViewSet):
         serializer.save(owner=self.request.user)
 
 
-# class FoundViewSet(viewsets.ReadOnlyModelViewSet):
-#     queryset = Found.objects.all()
-#     serializer_class = AnimalSerializer
-#     permission_classes = (AllowAny,)
+class LostProfileFilter(filters.FilterSet):
+    city = filters.CharFilter(field_name='animal__city', lookup_expr='iexact')
+    date = filters.DateFilter(field_name='loss_date', lookup_expr='lte')
+    species = filters.CharFilter(field_name='animal__species', lookup_expr='exact')
 
 
 class LostProfileViewSet(viewsets.ModelViewSet):
     queryset = LostProfile.objects.select_related('animal').all()
     serializer_class = LostProfileSerializer
     permission_classes = (AllowAny,)
+    filterset_class = LostProfileFilter
 
     def get_serializer_class(self):
         if self.request.method in ('GET', 'LIST'):
--- static/js/lost.js
+++ static/js/lost.js
@@ -2,18 +2,26 @@ export default {
     data() {
         return {
             profiles: null,
-            url: '/api/v1/lost_profiles/'
+            url: '/api/v1/lost_profiles/',
+            city: null,
+            date: null
         }
     },
-    props: {
-        city: String
-    },
-    async mounted() {
-        const requestOptions = { method: "GET" }
-        const response = await fetch(this.url, requestOptions)
-        this.profiles = await response.json()
+    methods: {
+        async detailsSubmit(e) {
+            if (this.date === null || this.city === null) { return }
+            const requestOptions = { method: "GET" }
+            const response = await fetch(`${this.url}?city=${this.city}&date=${this.date}`, requestOptions)
+            this.profiles = await response.json()
+        }
     },
     template: `
+    <div class="col-md-6">
+        <input v-model="city" type="text" placeholder="Город" class="form-control mb-4" @blur="detailsSubmit">
+    </div>
+    <div class="col-md-6">
+        <input v-model="date" type="date" class="form-control mb-4" @blur="detailsSubmit">
+    </div>
     <div v-for="profile in profiles" :key="profile.id" class="card col-md-6 col-xl-3">
         <img :src="profile.animal.avatar_thumbnail" class="card-img-top" alt="Фото животного">
         <div class="card-body text-center">
